39992b0ed2c189f4c5d30eafb98c49a2f9e8367f
aslivashu <sarthaktyagi1133@gmail.com>
2026-08-18 13:17:07 +0530

    installed session-parser and express-session

--- app.js
+++ app.js
@@ -6,21 +6,22 @@ const mongoose = require("mongoose");
 
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
+const cookieParser = require("cookie-parser");
 
 const ExpressError = require("./utils/ExpressError.js");
 
 const reviews = require("./routes/reviews.js");
 const listings = require("./routes/listing.js");
 
+
+
 // MongoDB connection
 const Mongo_URL = "mongodb://localhost:27017/HavenHop";
-
 
 // Connect to MongoDB
 main().then(() => {
     console.log ("connected to DB");
 }).catch(err => console.log(err));
-
 
 // Function to connect to MongoDB
 async function main(){
@@ -28,37 +29,37 @@ async function main(){
 }
 
 
+
 // Middleware to parse request bodies
 app.get("/", (req, res) => { 
-  res.send("Hello, World!");
+  res.send("Hi, welcome to HavenHop!");
 }); 
-
 
 // Set up EJS as the view engine and configure middleware
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
+app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 app.engine("ejs", ejsMate);
-app.use(express.static(path.join(__dirname, "public")));
+app.use(cookieParser());
 
 //listing routes
 app.use("/listings", listings);
 //review routes
 app.use("/listings/:id/reviews", reviews);
 
-
-
-
-app.all("/*splat", (req, res, next)=>{
-    next(new ExpressError(404, "Page Not Found"));
-});
-
-
+// Error handling middleware
 app.use((err, req, res, next)=>{
     const {statusCode=500, message= "Something went wrong"} = err;
     res.render("error.ejs", {statusCode, message});
 });
+
+// Catch-all route for handling 404 errors
+app.all("/*splat", (req, res, next)=>{
+    next(new ExpressError(404, "Page Not Found"));
+});
+
 
 
 //server listening
