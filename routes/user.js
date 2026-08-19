04a06a8581dccc06e3831b4fdb4d7f240b099ac7
aslivashu <sarthaktyagi1133@gmail.com>
2026-08-19 14:31:56 +0530

    Fix redirect handling after successful login and signup

--- routes/user.js
+++ routes/user.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const User = require("../models/user.js");
 const wrapAsync = require("../utils/wrapAsync.js");
 const passport = require("passport");
+const{saveRedirectUrl} = require("../middleware.js");
 
 // Route to render the signup page
 router.get("/signup", wrapAsync(async(req,res)=> {
@@ -10,7 +11,7 @@ router.get("/signup", wrapAsync(async(req,res)=> {
 }));
 
 // Route to handle user signup
-router.post("/signup", wrapAsync(async(req,res)=> {
+router.post("/signup", saveRedirectUrl, wrapAsync(async(req,res)=> {
     try{
         let {email, username, password} = req.body;   
         const newUser = new User({email, username});
@@ -21,7 +22,7 @@ router.post("/signup", wrapAsync(async(req,res)=> {
                 return next(err);
             }
             req.flash("success", "Welcome to HavenHop!");
-            res.redirect("/listings");  
+             res.redirect(res.locals.redirectUrl || "/listings");
          });
     } catch(err) {
         req.flash("error", err.message);
@@ -36,13 +37,14 @@ router.get("/login", wrapAsync(async(req,res)=> {
 
 // Route to handle user login
 router.post("/login",
+    saveRedirectUrl, 
     passport.authenticate("local", {
     failureRedirect: "/users/login",
     failureFlash: true,
 }), 
     wrapAsync(async(req,res)=> {
         req.flash("success", "Welcome back!");
-        res.redirect("/listings");
+        res.redirect(res.locals.redirectUrl || "/listings");
 }));
 
 // Route to handle user logout
